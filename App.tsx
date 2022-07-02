ccdeac2e5c215f5f8543dbd81ad0ac0f777ef7f5
suraj.s <suraj.s@gofrugal.com>
2022-07-02 17:26:15 +0530

    First commit

--- App.tsx
+++ App.tsx
@@ -11,8 +11,14 @@ import {
   extendTheme,
   VStack,
   Box,
+  FormControl,
+  Input,
+  Button,
 } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
+// import Loader from './Loader/loader';
+import LoginScreen from "react-native-login-screen";
+var LogoImage = require('./assets/tcs.png');
 
 // Define the config
 const config = {
@@ -29,41 +35,55 @@ declare module "native-base" {
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
+     <Center w="100%">
+      <Box safeArea p="2" py="8" w="90%" maxW="290">
+        <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
+        color: "warmGray.50"
+      }}>
+          Welcome to Karikalan Circus
+        </Heading>
+        <Heading mt="1" _dark={{
+        color: "warmGray.200"
+      }} color="coolGray.600" fontWeight="medium" size="xs">
+          Sign in to continue!
+        </Heading>
+
+        <VStack space={3} mt="5">
+          <FormControl>
+            <FormControl.Label>Email ID</FormControl.Label>
+            <Input />
+          </FormControl>
+          <FormControl>
+            <FormControl.Label>Password</FormControl.Label>
+            <Input type="password" />
+            <Link _text={{
+            fontSize: "xs",
+            fontWeight: "500",
+            color: "indigo.500"
+          }} alignSelf="flex-end" mt="1">
+              Forget Password?
+            </Link>
+          </FormControl>
+          <Button mt="2" colorScheme="indigo">
+            Sign in
+          </Button>
+          <HStack mt="6" justifyContent="center">
+            <Text fontSize="sm" color="coolGray.600" _dark={{
+            color: "warmGray.200"
+          }}>
+              I'm a new user.{" "}
             </Text>
-          </Link>
-          <ToggleDarkMode />
+            <Link _text={{
+            color: "indigo.500",
+            fontWeight: "medium",
+            fontSize: "sm"
+          }} href="#">
+              Sign Up
+            </Link>
+          </HStack>
         </VStack>
-      </Center>
+      </Box>
+    </Center>
     </NativeBaseProvider>
   );
 }
---
+++ Loader/loader.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import Animation from "react-native-lottie";
+import { Animated, Platform, StyleSheet } from "react-native";
+ 
+import SomeLottieAnimation from "../../assets/some-lottie-animation.json";
+ 
+const styles = StyleSheet.create({
+  anim: { width: 200, height: 200, backgroundColor: "green" },
+});
+ 
+export default () => {
+  const [progress] = useState(() => new Animated.Value(0));
+  useEffect(
+    () => {
+      Animated.timing(
+        progress,
+        { toValue: 1, duration: 1000, useNativeDriver: Platform.OS !== "web" },
+      ).start();
+    },
+    [progress],
+  );
+  return (
+    <Animation
+      style={styles.anim}
+      source={SomeLottieAnimation}
+      progress={progress}
+    />
+  );
+};
